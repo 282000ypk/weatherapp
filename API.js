2b1d097a0a84af43725f7012d25a30faf70d784b
yogesh <ypk282000@gmail.com>
2021-12-15 10:45:18 +0530

    loading animation added

--- API.js
+++ API.js
@@ -4,21 +4,27 @@ function update()
 {
 	let city=document.getElementById("city").value;
 	let country=document.getElementById("country").value;
+	if(city !== "" || country!== "")
+	{
+	document.querySelector(".loading").style.display = "flex"
 	fetch("https://weatherbit-v1-mashape.p.rapidapi.com/current?&city="+city+"&country="+country+"&lang=en", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "f43d8a7e02mshb001ea2faa3b125p177b37jsn3a0a9ec6f18c",
 		"x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com"
 	}
-})
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  document.getElementById("output").innerHTML="Temperature: "+data.data[0].temp+" <sup>o</sup>C";
-})
-.catch(err => {
+	})
+	.then(response => response.json())
+	.then(data => {
+	document.querySelector(".loading").style.display = "none"
+  	console.log(data);
+  	document.querySelector(".output").innerHTML="Temperature: "+data.data[0].temp+" <sup>o</sup>C";
+	})
+	.catch(err => {
 	console.error(err);
-	document.getElementById("output").innerText="Please enter a valid city and country name"
-});	
+	document.querySelector(".loading").style.display = "none"
+	document.querySelector(".output").innerText="Please enter a valid city and country name"
+	});	
+	}
 }
 
